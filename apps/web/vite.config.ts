81358d46c0122fa232fb6c1274ed50ac7012312a
Blackmamoth <blackmamoth83@gmail.com>
2025-12-22 18:15:03 +0530

    add nitro plugin to deploy on vercel

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,7 @@
-import netlify from "@netlify/vite-plugin-tanstack-start";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -11,7 +11,7 @@ export default defineConfig({
 		tailwindcss(),
 		tanstackStart(),
 		viteReact(),
-		netlify(),
+		nitro(),
 	],
 	ssr: {
 		external: ["esbuild"],
